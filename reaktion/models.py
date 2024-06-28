1de9c3effd33c6ccf84469bbb3423a2ba6aa58d1
jhnnsrs <jhnnsrs@gmail.com>
2024-06-28 16:46:41 +0200

    ups

--- reaktion/models.py
+++ reaktion/models.py
@@ -1,20 +1,23 @@
-from django.db import models
+import uuid
+
+from authentikate import App
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 from django.db import models
-from django.contrib.auth import get_user_model
-import uuid
-from authentikate import App
 
 # Create your models here.
 from django_choices_field import TextChoicesField
+
 from . import enums
 
 
 class Room(models.Model):
     """Room is a Template for a Template"""
+
     restrict = models.JSONField(
-        default=list, help_text="Restrict access to specific nodes for this diagram okay?"
+        default=list,
+        help_text="Restrict access to specific nodes for this diagram okay?",
     )
     title = models.CharField(max_length=10000, null=True)
     description = models.CharField(max_length=10000, null=True)
@@ -30,9 +33,6 @@ class Room(models.Model):
 
     def __str__(self):
         return f"{self.title}"
-    
-
-    
 
 
 class Agent(models.Model):
@@ -42,26 +42,22 @@ class Agent(models.Model):
     user = models.ForeignKey(User, max_length=4000)
 
 
-
-
-
 class Portal(models.Model):
-    """ A portal is a media outlet 
-    
+    """A portal is a media outlet
+
     that allows to permamently attach media to a room
     a port can be pinned by users to appear in close
     proximity
-    
+
     """
+
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     causing_agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
     open = models.BooleanField(default=True, help_text="Is the portal still open")
 
 
 class StreamPortal(models.Model):
-
-
-
+    pass
 
 
 class Structure(models.Model):
@@ -71,6 +67,11 @@ class Structure(models.Model):
 
 class Message(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    agent =  models.ForeignKey(Agent, on_delete=models.CASCADE)
-    is_streaming = models.BooleanField(default=False, help_text="Is the message currently streaming? Will mark it as streaming on the webinterface")
-    attached_structures = models.ManyToManyField(Structure, help_text="Does this message ")
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    is_streaming = models.BooleanField(
+        default=False,
+        help_text="Is the message currently streaming? Will mark it as streaming on the webinterface",
+    )
+    attached_structures = models.ManyToManyField(
+        Structure, help_text="Does this message "
+    )
